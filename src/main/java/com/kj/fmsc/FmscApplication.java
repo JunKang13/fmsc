526e8ca145fc64d3efb086d82f4c92fbe6d1698e
Jun Kang <junkang13@ucla.edu>
2024-06-22 16:47:21 -0700

    Init

---
+++ src/main/java/com/kj/fmsc/FmscApplication.java
@@ -0,0 +1,13 @@
+package com.kj.fmsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kj/fmsc/FmscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj.fmsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
